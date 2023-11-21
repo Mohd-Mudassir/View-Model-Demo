67624365f37ba77f6560ee7c872ef17620800079
Mohd.mudassir <mohd.mudassir@srmtechsol.com>
2023-11-21 11:10:16 +0530

    First Ever Commit

---
+++ app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.anushka.viewmodeldemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.anushka.viewmodeldemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    //private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        //binding.countText.text = count.toString()
+
+        binding.countText.text = viewModel.getCurrentCount().toString()
+
+        binding.button.setOnClickListener {
+            //count++
+
+            binding.countText.text = viewModel.getUpdatedCount().toString()
+        }
+    }
+}
